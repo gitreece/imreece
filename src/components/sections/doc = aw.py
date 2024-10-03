abd8c1b8816073c443beab9ca2049a208df6f65c
Mauricio Pereira <45366787+gitreece@users.noreply.github.com>
2024-10-03 14:58:51 -0400

    5 NEW changes

---
+++ src/components/sections/doc = aw.py
@@ -0,0 +1,12 @@
+doc = aw.Document()
+builder = aw.DocumentBuilder(doc)
+
+builder.writeln("Hello _CustomerName_,")
+print("Original document text: " + doc.range.text)
+
+doc.range.replace("_CustomerName_", "James Bond", aw.replacing.FindReplaceOptions(aw.replacing.FindReplaceDirection.FORWARD))
+
+print("Document text after replace: " + doc.range.text)
+
+# Save the modified document
+doc.save(docs_base.artifacts_dir + "FindAndReplace.simple_find_replace.docx")
